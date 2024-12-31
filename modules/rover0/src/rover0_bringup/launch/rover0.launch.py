fc6b96a8aa3b86444744076f6fd659011107050f
Masahiro Wada <argon.argon.argon@gmail.com>
2024-12-31 22:36:10 +0000

    use turtlebot3 map

--- modules/rover0/src/rover0_bringup/launch/rover0.launch.py
+++ modules/rover0/src/rover0_bringup/launch/rover0.launch.py
@@ -1,35 +1,25 @@
+import os
+import tempfile
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import AppendEnvironmentVariable
 from launch import LaunchDescription, LaunchDescriptionEntity, condition
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.descriptions import ParameterValue
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, IfElseSubstitution, PythonExpression
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition, UnlessCondition
-
-def get_robot_description(use_sim: bool):
-    package_name = "rover0_gz_description" if use_sim else "rover0_robot_description"
-    xacro_file_name = "rover0_gz.urdf.xacro" if use_sim else "rover0_robot.urdf.xacro"
-
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare(package_name), "urdf", xacro_file_name
-                ]
-            ),
-        ]
-    )
-    return {"robot_description": ParameterValue(robot_description_content, value_type=str)}
+from launch_ros.substitutions.find_package import FindPackage
+from launch.event_handlers import OnShutdown
 
 def generate_launch_description():
-    default_world_file_path = PathJoinSubstitution([
-        FindPackageShare('rover0_gz_description'),
-        'world',
-        'my_world.sdf'
-    ])
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_dir2 = get_package_share_directory('nav2_minimal_tb3_sim')
+    sim_dir = get_package_share_directory('nav2_minimal_tb3_sim')
 
     declared_arguments = [
         DeclareLaunchArgument(
@@ -38,30 +28,57 @@ def generate_launch_description():
             description='Prefix to be added to the robot description'
         ),
         DeclareLaunchArgument(
-            'world_sdf_file',
-            default_value=default_world_file_path,
-            description='Path to the SDF world file'
+            'map',
+            default_value=os.path.join(bringup_dir, 'maps', 'tb3_sandbox.yaml'),
+        ),
+        DeclareLaunchArgument(
+            'world',
+            default_value=os.path.join(sim_dir, 'worlds', 'tb3_sandbox.sdf.xacro'),
+            description='Full path to world model file to load',
         ),
         DeclareLaunchArgument(
             'use_sim',
-            default_value='false',
+            default_value='False',
             description='Use gazebo sim'
         ),
         DeclareLaunchArgument(
             'use_rviz',
-            default_value='false',
+            default_value='False',
             description='Use rviz'
+        ),
+        DeclareLaunchArgument(
+            'headless', default_value='False', description='Whether to execute gzclient)'
         )
     ]
 
     prefix = LaunchConfiguration('prefix')
-    world_sdf_file = LaunchConfiguration('world_sdf_file')
+    map_yaml_file = LaunchConfiguration('map')
+    world = LaunchConfiguration('world')
     use_sim = LaunchConfiguration('use_sim')
     use_rviz = LaunchConfiguration('use_rviz')
+    headless = LaunchConfiguration('headless')
+    pose = {'x': LaunchConfiguration('x_pose', default='-2.00'),
+            'y': LaunchConfiguration('y_pose', default='-0.50'),
+            'z': LaunchConfiguration('z_pose', default='0.01'),
+            'R': LaunchConfiguration('roll', default='0.00'),
+            'P': LaunchConfiguration('pitch', default='0.00'),
+            'Y': LaunchConfiguration('yaw', default='0.00')}
 
-    print(use_sim)
+    package_name = IfElseSubstitution(use_sim, if_value="rover0_gz_description", else_value="rover0_robot_description")
+    model_xacro_file_name = IfElseSubstitution(use_sim, if_value="rover0_gz.urdf.xacro", else_value="rover0_robot.urdf.xacro")
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare(package_name), "urdf", model_xacro_file_name
+                ],
+            ),
+        ]
+    )
+    robot_description = {"robot_description": ParameterValue(robot_description_content, value_type=str)}
 
-    robot_description = get_robot_description(use_sim)
 
     robot_state_pub_node = Node(
         package="robot_state_publisher",
@@ -74,11 +91,13 @@ def generate_launch_description():
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
+        condition=UnlessCondition(use_sim)
     )
     imu_sensor_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["imu_sensor_broadcaster", "-c", "/controller_manager"],
+        condition=UnlessCondition(use_sim)
     )
 
     rover0_controller_spawner = Node(
@@ -128,14 +147,29 @@ def generate_launch_description():
         output="both"
     )
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            FindPackageShare('ros_gz_sim').find('ros_gz_sim'),
-            '/launch/gz_sim.launch.py'
-        ]),
-        condition=IfCondition(use_sim),
-        launch_arguments={'gz_args': ['-r ', world_sdf_file],  'on_exit_shutdown': 'true'}.items()
+    world_sdf = tempfile.mktemp(prefix='nav2_', suffix='.sdf')
+    world_sdf_xacro = ExecuteProcess(
+        cmd=['xacro', '-o', world_sdf, ['headless:=', headless], world])
+    gazebo_server = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', world_sdf],
+        output='screen',
+        condition=IfCondition(use_sim)
     )
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'),
+                         'launch',
+                         'gz_sim.launch.py')
+        ),
+        condition=IfCondition(PythonExpression(
+            [use_sim, ' and not ', headless])),
+        launch_arguments={'gz_args': ['-v4 -g ']}.items(),
+    )
+
+    remove_temp_sdf_file = RegisterEventHandler(event_handler=OnShutdown(
+        on_shutdown=[
+            OpaqueFunction(function=lambda _: os.remove(world_sdf))
+        ]))
 
     ros_gz_bridge_config_file = PathJoinSubstitution(
         [FindPackageShare("rover0_gz_description"), "config", "rover0_bridge.yaml"]
@@ -145,7 +179,9 @@ def generate_launch_description():
         package='ros_gz_bridge',
         executable='parameter_bridge',
         parameters=[{
-            'config_file': ros_gz_bridge_config_file
+            'config_file': ros_gz_bridge_config_file,
+            'expand_gz_topic_names': True,
+            'use_sim_time': True,
         }],
         output='screen'
     )
@@ -157,11 +193,21 @@ def generate_launch_description():
       arguments=[
           '-topic', '/robot_description',
           '-name', 'rover0',
-          "-z", '0.01',
+          '-x', pose['x'], '-y', pose['y'], '-z', pose['z'],
+          '-R', pose['R'], '-P', pose['P'], '-Y', pose['Y']
       ],
       output='screen'
     )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH', os.path.join(bringup_dir2, 'models'))
+    set_env_vars_resources2 = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            str(Path(os.path.join(bringup_dir2)).parent.resolve()))
+
     nodes: list[LaunchDescriptionEntity]= [
+        set_env_vars_resources,
+        set_env_vars_resources2,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         imu_sensor_broadcaster_spawner,
@@ -169,9 +215,12 @@ def generate_launch_description():
         imu_filter_madgwick_node,
         rviz_node,
         ros2_control_node,
-        gazebo,
+        world_sdf_xacro,
+        gazebo_server,
+        gazebo_client,
+        remove_temp_sdf_file,
         ros_gz_bridge_node,
-        spawn_entity_node
+        spawn_entity_node,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
